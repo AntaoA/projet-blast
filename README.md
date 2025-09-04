a1fd1983d78d1e77bdebdfe98ac702a01f9eabac
AntaoA <amoryantao@net-c.com>
2025-09-04 13:32:50 +0200

    Choses à faire + matrices pam250 et blosum62

---
+++ README.md
@@ -0,0 +1,6 @@
+Pour utiliser ce projet, vous devez d'abord installer uv sur votre machine. Ensuite, vous pouvez cloner ce dépôt et utiliser les commandes uv pour gérer l'environnement et les dépendances.
+
+
+git clone https://github.com/pierrepo/projet-blast
+cd demo-uv-pp
+uv sync
---
+++ blast.py
@@ -0,0 +1,9 @@
+# Blast :
+#   1. Separer une sequence en mots de taille k
+#   2. Trouver dans l'autre sequence s'il y a des correspondances (via matrice de score)
+#   3. Etendre les correspondances pour trouver des alignements locaux (gauche et droite)
+#   4. Garder les alignements avec un score superieur a un certain seuil
+#   5. Calculer la significativite des alignements (E-value)
+#   6. Afficher les alignements significatifs
+
+# Extension BLAST II
