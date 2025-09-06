1fc10e8f52256c4aff1cbfb19c37429d62fa9b01
AntaoA <amoryantao@net-c.com>
2025-09-06 12:43:22 +0200

    Version basique, proteines random

--- blast.py
+++ blast.py
@@ -17,7 +17,7 @@ def construire_dico_mots(seq, w):
 
 
 def find_hits(dico_mots, database, w):
-    hits = []
+    hits = set()
     for seq_id, seq in enumerate(database):
         nb_mots_db = len(seq) - w + 1
         # Parcours des mots de la séquence de la database
@@ -25,55 +25,117 @@ def find_hits(dico_mots, database, w):
             mot_db = seq[j:j+w]
             if mot_db in dico_mots:
                 for pos in dico_mots[mot_db]:
-                    hits.append((seq_id, pos, j))  # (id_sequence, position_seq1, position_seq2)
+                    hits.add((seq_id, pos, j))  # (id_sequence, position_seq1, position_seq2)
     return hits
 
 
-def extend_hit(hit, sequence, database, w, matrice_substitution, seuil):
-    data_id, pos_seq, pos_data = hit
-    score = sum(matrice_substitution[sequence[pos_seq + i]][database[data_id][pos_data + i]] for i in range(w))
-    max_score = score
+def extend_hit(hit, sequence, database, w, M, x_drop=20):
+    data_id, pos_q0, pos_s0 = hit
 
-    qL, qR = pos_seq, pos_seq + w - 1
-    sL, sR = pos_data, pos_data + w - 1
-    
-    # Extension à droite
-    pos_seq = qR
-    pos_data = sR
+    # score du seed (w-mer)
+    seed_score = sum(M[sequence[pos_q0+i]][database[data_id][pos_s0+i]] for i in range(w))
 
-    while max_score - score <= seuil and pos_seq + 1 < len(sequence) and pos_data + 1 < len(database[data_id]):
-        pos_seq += 1
-        pos_data += 1
-        score += matrice_substitution[sequence[pos_seq]][database[data_id][pos_data]]
-        if score > max_score:
-            max_score = score
-            qR = pos_seq
-            sR = pos_data
-    
-    # Extension à gauche
-    pos_seq = qL
-    pos_data = sL
-    score = max_score
-    
-    while max_score - score <= seuil and pos_seq - 1 >= 0 and pos_data - 1 >= 0:
-        pos_seq -= 1
-        pos_data -= 1
-        score += matrice_substitution[sequence[pos_seq]][database[data_id][pos_data]]
-        if score > max_score:
-            max_score = score
-            qL = pos_seq
-            sL = pos_data
+    # bornes initiales = seed
+    qL = pos_q0
+    sL = pos_s0
+    qR = pos_q0 + w - 1
+    sR = pos_s0 + w - 1
 
-    return (qL, sL, qR, sR, max_score)
+    # --------- Extension à droite (X-drop local) ----------
+    score = seed_score
+    best_r = seed_score
+    best_qR, best_sR = qR, sR
 
+    q = qR; s = sR
+    while q + 1 < len(sequence) and s + 1 < len(database[data_id]):
+        q += 1; s += 1
+        score += M[sequence[q]][database[data_id][s]]
+
+        if score > best_r:
+            best_r = score
+            best_qR, best_sR = q, s
+
+        if best_r - score > x_drop:
+            break
+
+    # --------- Extension à gauche (X-drop local) ----------
+    score = seed_score
+    best_l = seed_score
+    best_qL, best_sL = qL, sL
+
+    q = qL; s = sL
+    while q - 1 >= 0 and s - 1 >= 0:
+        q -= 1; s -= 1
+        score += M[sequence[q]][database[data_id][s]]
+
+        if score > best_l:
+            best_l = score
+            best_qL, best_sL = q, s
+
+        if best_l - score > x_drop:
+            break
+
+    # --------- Combinaison des gains ----------
+    # total_best = seed + gain_droite + gain_gauche
+    total_best = seed_score + (best_r - seed_score) + (best_l - seed_score)
+
+    return (best_qL, best_sL, best_qR, best_sR, total_best)
+
+
+
+
+# Protéines
+
+def construire_mots_initiaux(sequence, w):
+    mots_initiaux = {}
+    nb_mots = len(sequence) - w + 1
+    for i in range(nb_mots):
+        mot = sequence[i:i+w]
+        if mot not in mots_initiaux:
+            mots_initiaux[mot] = []
+        mots_initiaux[mot].append(i)
+    return mots_initiaux
+
+def generate_neighbors(mot, matrice_substitution, seuil_t):
+    score = sum(matrice_substitution[lettre][lettre] for lettre in mot)
+    voisins = []
+    w = len(mot)
+    if score >= seuil_t:
+        for i in range(w):
+            score_ligne = matrice_substitution[mot[i]]
+            score_ligne = dict(sorted(score_ligne.items(), key=lambda item: item[1], reverse=True))
+            for lettre, score_lettre in score_ligne.items():
+                if lettre != mot[i]:
+                    nouveau_mot = mot[:i] + lettre + mot[i+1:]
+                    nouveau_score = score - matrice_substitution[mot[i]][mot[i]] + score_lettre
+                    if nouveau_score >= seuil_t:
+                        if nouveau_mot not in voisins:
+                            voisins.append(nouveau_mot)
+                    else:
+                        break
+    return voisins
+
+def construire_liste_voisins(sequence, w, matrice_substitution, seuil_t):
+    mots_initiaux = construire_mots_initiaux(sequence, w)
+    voisins = {}
+    for mot in mots_initiaux:
+        pos = mots_initiaux[mot]
+        liste_voisins = generate_neighbors(mot, matrice_substitution, seuil_t)
+        for voisin in liste_voisins:
+            if voisin not in voisins:
+                voisins[voisin] = set()
+            voisins[voisin].update(pos)
+
+    return voisins
 
 if __name__ == "__main__":
-    
+    # ADN aléatoire
     # Paramètres
-
-    w = 11  # taille des mots
+    '''
+    w = 13  # taille des mots
     n = 500 # taille de la séquence
     nb_mots = n - w + 1
+    x_drop = 20
     seuil = 50 # seuil de score pour garder un alignement
     taille_database = 1000 # taille de la base de données
     database = [''.join(random.choices('ACGT', k=random.randint(1000, 5000))) for _ in range(taille_database)]
@@ -98,13 +160,14 @@ if __name__ == "__main__":
     alignements = {}
     
     for hit in hits:
-        alignment = extend_hit(hit, sequence, database, w, matrice_substitution, seuil)
+        alignment = extend_hit(hit, sequence, database, w, matrice_substitution, x_drop)
         qL, sL, qR, sR, score = alignment
-        if score not in alignements:
-            alignements[score] = []
-        alignements[score].append((qL, sL, qR, sR, hit[0]))
-
         if score >= seuil:
+
+            if score not in alignements:
+                alignements[score] = set()
+            alignements[score].add((qL, sL, qR, sR, hit[0]))
+
             print(f"Alignement trouvé dans la séquence {hit[0]} avec un score de {score}")
             print(f"Sequence 1: {sequence[qL:qR+1]} (positions {qL} à {qR})")
             print(f"Sequence 2: {database[hit[0]][sL:sR+1]} (positions {sL} à {sR})")
@@ -113,3 +176,94 @@ if __name__ == "__main__":
 
     alignements = dict(sorted(alignements.items(), reverse=True))
     print(alignements)
+    '''
+    
+    
+    
+    # Protéines aléatoires
+    # Paramètres
+    alphabete_proteines = 'ACDEFGHIKLMNPQRSTVWY'
+    
+    w = 4     # taille des mots
+    n = 500 # taille de la séquence
+    seuil_t = 17 # seuil de score pour garder un alignement
+    seuil = 80 # seuil de score pour garder un alignement
+    x_drop = 20
+    taille_database = 1000 # taille de la base de données
+    database = [''.join(random.choices(alphabete_proteines, k=random.randint(1000, 5000))) for _ in range(taille_database)]
+
+    sequence = ''.join(random.choices(alphabete_proteines, k=n))
+
+    PAM250 = {
+        'A': {'A':  2, 'R': -2, 'N':  0, 'D':  0, 'C': -2, 'Q':  0, 'E':  0, 'G':  1, 'H': -1, 'I': -1,
+            'L': -2, 'K': -1, 'M': -1, 'F': -3, 'P':  1, 'S':  1, 'T':  1, 'W': -6, 'Y': -3, 'V':  0},
+        'R': {'A': -2, 'R':  6, 'N':  0, 'D': -1, 'C': -4, 'Q':  1, 'E': -1, 'G': -3, 'H':  2, 'I': -2,
+            'L': -3, 'K':  3, 'M':  0, 'F': -4, 'P':  0, 'S':  0, 'T': -1, 'W':  2, 'Y': -4, 'V': -2},
+        'N': {'A':  0, 'R':  0, 'N':  2, 'D':  2, 'C': -4, 'Q':  1, 'E':  1, 'G':  0, 'H':  2, 'I': -2,
+            'L': -3, 'K':  1, 'M': -2, 'F': -3, 'P':  0, 'S':  1, 'T':  0, 'W': -4, 'Y': -2, 'V': -2},
+        'D': {'A':  0, 'R': -1, 'N':  2, 'D':  4, 'C': -5, 'Q':  2, 'E':  3, 'G':  1, 'H':  1, 'I': -2,
+            'L': -4, 'K':  0, 'M': -3, 'F': -6, 'P': -1, 'S':  0, 'T':  0, 'W': -7, 'Y': -4, 'V': -2},
+        'C': {'A': -2, 'R': -4, 'N': -4, 'D': -5, 'C': 12, 'Q': -5, 'E': -5, 'G': -3, 'H': -3, 'I': -2,
+            'L': -6, 'K': -5, 'M': -5, 'F': -4, 'P': -3, 'S':  0, 'T': -2, 'W': -8, 'Y':  0, 'V': -2},
+        'Q': {'A':  0, 'R':  1, 'N':  1, 'D':  2, 'C': -5, 'Q':  4, 'E':  2, 'G': -1, 'H':  3, 'I': -2,
+            'L': -2, 'K':  1, 'M': -1, 'F': -5, 'P':  0, 'S': -1, 'T': -1, 'W': -5, 'Y': -4, 'V': -2},
+        'E': {'A':  0, 'R': -1, 'N':  1, 'D':  3, 'C': -5, 'Q':  2, 'E':  4, 'G':  0, 'H':  1, 'I': -2,
+            'L': -3, 'K':  0, 'M': -2, 'F': -5, 'P': -1, 'S':  0, 'T':  0, 'W': -7, 'Y': -4, 'V': -2},
+        'G': {'A':  1, 'R': -3, 'N':  0, 'D':  1, 'C': -3, 'Q': -1, 'E':  0, 'G':  5, 'H': -2, 'I': -3,
+            'L': -4, 'K': -2, 'M': -3, 'F': -5, 'P':  0, 'S':  1, 'T':  0, 'W': -7, 'Y': -5, 'V': -1},
+        'H': {'A': -1, 'R':  2, 'N':  2, 'D':  1, 'C': -3, 'Q':  3, 'E':  1, 'G': -2, 'H':  6, 'I': -2,
+            'L': -2, 'K':  0, 'M': -2, 'F': -2, 'P':  0, 'S': -1, 'T': -1, 'W': -3, 'Y':  0, 'V': -2},
+        'I': {'A': -1, 'R': -2, 'N': -2, 'D': -2, 'C': -2, 'Q': -2, 'E': -2, 'G': -3, 'H': -2, 'I':  5,
+            'L':  2, 'K': -2, 'M':  2, 'F':  1, 'P': -2, 'S': -1, 'T':  0, 'W': -5, 'Y': -1, 'V':  4},
+        'L': {'A': -2, 'R': -3, 'N': -3, 'D': -4, 'C': -6, 'Q': -2, 'E': -3, 'G': -4, 'H': -2, 'I':  2,
+            'L':  6, 'K': -3, 'M':  4, 'F':  2, 'P': -3, 'S': -3, 'T': -2, 'W': -2, 'Y': -1, 'V':  2},
+        'K': {'A': -1, 'R':  3, 'N':  1, 'D':  0, 'C': -5, 'Q':  1, 'E':  0, 'G': -2, 'H':  0, 'I': -2,
+            'L': -3, 'K':  5, 'M':  0, 'F': -5, 'P': -1, 'S':  0, 'T':  0, 'W': -3, 'Y': -4, 'V': -2},
+        'M': {'A': -1, 'R':  0, 'N': -2, 'D': -3, 'C': -5, 'Q': -1, 'E': -2, 'G': -3, 'H': -2, 'I':  2,
+            'L':  4, 'K':  0, 'M':  6, 'F':  0, 'P': -2, 'S': -2, 'T': -1, 'W': -4, 'Y': -2, 'V':  2},
+        'F': {'A': -3, 'R': -4, 'N': -3, 'D': -6, 'C': -4, 'Q': -5, 'E': -5, 'G': -5, 'H': -2, 'I':  1,
+            'L':  2, 'K': -5, 'M':  0, 'F':  9, 'P': -5, 'S': -3, 'T': -3, 'W':  0, 'Y':  7, 'V': -1},
+        'P': {'A':  1, 'R':  0, 'N':  0, 'D': -1, 'C': -3, 'Q':  0, 'E': -1, 'G':  0, 'H':  0, 'I': -2,
+            'L': -3, 'K': -1, 'M': -2, 'F': -5, 'P':  6, 'S':  1, 'T':  0, 'W': -6, 'Y': -5, 'V': -1},
+        'S': {'A':  1, 'R':  0, 'N':  1, 'D':  0, 'C':  0, 'Q': -1, 'E':  0, 'G':  1, 'H': -1, 'I': -1,
+            'L': -3, 'K':  0, 'M': -2, 'F': -3, 'P':  1, 'S':  2, 'T':  1, 'W': -2, 'Y': -3, 'V': -1},
+        'T': {'A':  1, 'R': -1, 'N':  0, 'D':  0, 'C': -2, 'Q': -1, 'E':  0, 'G':  0, 'H': -1, 'I':  0,
+            'L': -2, 'K':  0, 'M': -1, 'F': -3, 'P':  0, 'S':  1, 'T':  3, 'W': -5, 'Y': -3, 'V':  0},
+        'W': {'A': -6, 'R':  2, 'N': -4, 'D': -7, 'C': -8, 'Q': -5, 'E': -7, 'G': -7, 'H': -3, 'I': -5,
+            'L': -2, 'K': -3, 'M': -4, 'F':  0, 'P': -6, 'S': -2, 'T': -5, 'W': 17, 'Y':  0, 'V': -6},
+        'Y': {'A': -3, 'R': -4, 'N': -2, 'D': -4, 'C':  0, 'Q': -4, 'E': -4, 'G': -5, 'H':  0, 'I': -1,
+            'L': -1, 'K': -4, 'M': -2, 'F':  7, 'P': -5, 'S': -3, 'T': -3, 'W':  0, 'Y': 10, 'V': -2},
+        'V': {'A':  0, 'R': -2, 'N': -2, 'D': -2, 'C': -2, 'Q': -2, 'E': -2, 'G': -1, 'H': -2, 'I':  4,
+            'L':  2, 'K': -2, 'M':  2, 'F': -1, 'P': -1, 'S': -1, 'T':  0, 'W': -6, 'Y': -2, 'V':  4}
+    }
+    
+    
+    
+    
+    liste = construire_liste_voisins(sequence, w, PAM250, seuil_t)
+    print(f"Nombre de voisins générés : {len(liste)}")
+    
+    hits = find_hits(liste, database, w)
+    print(f"Nombre de hits trouvés : {len(hits)}")
+    
+    alignements = {}
+        
+    for hit in hits:
+        alignment = extend_hit(hit, sequence, database, w, PAM250, x_drop)
+        qL, sL, qR, sR, score = alignment
+        if score >= seuil:
+
+            if score not in alignements:
+                alignements[score] = set()
+            alignements[score].add((qL, sL, qR, sR, hit[0]))
+
+            print(f"Alignement trouvé dans la séquence {hit[0]} avec un score de {score}")
+            print(f"Sequence 1: {sequence[qL:qR+1]} (positions {qL} à {qR})")
+            print(f"Sequence 2: {database[hit[0]][sL:sR+1]} (positions {sL} à {sR})")
+            print()
+
+
+    alignements = dict(sorted(alignements.items(), reverse=True))
+    print(len(alignements))
+    for score in alignements:
+        print(f"Score: {score}, Nombre d'alignements: {len(alignements[score])}")
